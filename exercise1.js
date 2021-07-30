8a5665642ed5fdb8e98d900283f8893919e1f9a5
VietAnh <vanhuwu82@gmail.com>
2021-07-30 21:31:23 +0700

    exercise1.js

---
+++ exercise1.js
@@ -0,0 +1,134 @@
+// có 3 quả chuối trong rổ nam ăn hết 2 quả;
+// xe chở chuối có 15 quả chuối làm rơi mất 5 quả;
+// (số nguyên)
+
+var banana_trong_ro = 3;
+banana_trong_ro = banana_trong_ro - 2;
+var banana_tren_xe = 15;
+banana_tren_xe = banana_tren_xe - 5;
+var sum_banana = banana_trong_ro + banana_tren_xe;
+console. log(sum_banana);
+
+// 1 can xăng đựng đc 67,23 ml nước, đổ xăng trào mất 23,44 ml;
+// thùng xăng chứa đc 120,20 ml nước, Viet Anh làm đổ mất 24,34 ml;
+// (số thập phân)
+   
+var can_xang = 67.23;
+can_xang = can_xang - 23.44;
+var thung_xang = 120.20;
+thung_xang = thung_xang  - 24.34;
+var ml_nuoc = can_xang + thung_xang;
+console. log(ml_xang);
+
+// string
+var Age = 16;
+var address = "Uong Bi Thanh Son";
+
+// boolean
+var
+
+
+// const
+// trong luong cay cau chiu duoc la 3 tan;
+// số cay but trong 1 hop là 20;
+// con duong nay chiu duoc 30 tan;
+// cay cau o Uong Bi có chiều cao an toàn là 10m;
+const MAX_tan = 3;
+const MAX_but_trong_hop = 30;
+const MAX_tan = 30;
+const MAX_M = ;
+
+
+
+
+// +,-,*,/,%;
+
+// số nguyên;
+var x = 4;
+var y = 6;
+console. log(y - x);
+// số thập phân;
+var x = 4.6
+var y = 2.3
+console. log(x - y);
+
+
+// số nguyên;
+var x = 6;
+var y = 3;
+console.log(x + y);
+// số thập phân;
+var x = 60.4
+var y = 92.6
+console. log(x + y);
+
+
+// số nguyên;
+var x = 4;
+var y = 2;
+console. log(4 * 2);
+// số thập phân
+var x = 57.5;
+var y = 12.4;
+console. log(x * y);
+
+// số nguyên
+var x = 10;
+var y = 5;
+console. log(x / y);
+// số thập phân
+var x = 35.2;
+var y = 23.9;
+console. log(x / y);
+
+// số nguyên
+var x = 60;
+var y = 40;
+console. log(x % y);
+// só thập phân
+var x = 27.6;
+var y = 14.6;
+console. log(x % y);
+
+// nối 2 biến string;
+var x = 'viet anh';
+var y = "retard";
+var c = x + '' + y;
+console. log(c);
+
+
+// gán
+var a = 36;
+var b = a;
+console. log(b);
+
+// ++, --;
+var x = 92;
+x++;
+console. log(x);
+
+var d = 21;
+d--;
+console. log(d);
+
+// tăng 10 lần và giảm 5 lần
+var t = 30;
+t +=10;
+console. log(t);
+
+var y = 42;
+y -=5;
+console. log(y);
+
+
+
+
+
+
+
+
+
+
+
+
+
