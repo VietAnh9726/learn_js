1ece417a373913f8b27915a047bacb806f59d208
VietAnh9726 <vanhuwu82@gmail.com>
2021-08-06 06:19:37 -0700

    exercise1.js

--- exercise1.js
+++ exercise1.js
@@ -18,7 +18,7 @@ can_xang = can_xang - 23.44;
 var thung_xang = 120.20;
 thung_xang = thung_xang  - 24.34;
 var ml_nuoc = can_xang + thung_xang;
-console. log(ml_xang);
+console.log(ml_xang);
 
 // string
 var Age = 16;
@@ -46,11 +46,11 @@ const MAX_M = ;
 // số nguyên;
 var x = 4;
 var y = 6;
-console. log(y - x);
+console.log(y - x);
 // số thập phân;
 var x = 4.6
 var y = 2.3
-console. log(x - y);
+console.log(x - y);
 
 
 // số nguyên;
@@ -60,65 +60,65 @@ console.log(x + y);
 // số thập phân;
 var x = 60.4
 var y = 92.6
-console. log(x + y);
+console.log(x + y);
 
 
 // số nguyên;
 var x = 4;
 var y = 2;
-console. log(4 * 2);
+console.log(4 * 2);
 // số thập phân
 var x = 57.5;
 var y = 12.4;
-console. log(x * y);
+console.log(x * y);
 
 // số nguyên
 var x = 10;
 var y = 5;
-console. log(x / y);
+console.log(x / y);
 // số thập phân
 var x = 35.2;
 var y = 23.9;
-console. log(x / y);
+console.log(x / y);
 
 // số nguyên
 var x = 60;
 var y = 40;
-console. log(x % y);
+console.log(x % y);
 // só thập phân
 var x = 27.6;
 var y = 14.6;
-console. log(x % y);
+console.log(x % y);
 
 // nối 2 biến string;
 var x = 'viet anh';
 var y = "retard";
 var c = x + '' + y;
-console. log(c);
+console.log(c);
 
 
 // gán
 var a = 36;
 var b = a;
-console. log(b);
+console.log(b);
 
 // ++, --;
 var x = 92;
 x++;
-console. log(x);
+console.log(x);
 
 var d = 21;
 d--;
-console. log(d);
+consoleloglog(d);
 
 // tăng 10 lần và giảm 5 lần
 var t = 30;
 t +=10;
-console. log(t);
+console.log(t);
 
 var y = 42;
 y -=5;
-console. log(y);
+console.log(y);
 
 
 
